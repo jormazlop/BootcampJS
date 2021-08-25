479bf15d4a7c01acb4a9c539db4dc0fbe4fc863e
jormazlop <jormazlop@gmail.com>
2021-08-25 10:32:51 +0200

    Entregable 1

---
+++ Modulo 1/js/console.js
@@ -0,0 +1,10 @@
+function mostrarName() {
+
+      var name = document.getElementById('name').value;
+
+      if(!name) {
+            console.log('Name esta vacio');
+      } else {
+            console.log('Name : ' , document.getElementById('name').value);
+      }
+}
---
+++ Modulo 1/js/update.js
@@ -0,0 +1,5 @@
+function updateCampos() {
+      document.getElementById('name').value = 'Jorge';
+      document.getElementById('lastName').value = 'Maza';
+      document.getElementById('avatar').src = './img/avatar.svg'
+}
